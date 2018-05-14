f3acb66a40c959088bd3999fa96d4d93cb7d1e9b
ongoni <ongoni.fg@gmail.com>
2018-05-14 19:20:13 +0400

    add controllers

---
+++ src/main/kotlin/com/ongoni/onlinebank/controller/UserController.kt
@@ -0,0 +1,9 @@
+package com.ongoni.onlinebank.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/user")
+class UserController {
+}
