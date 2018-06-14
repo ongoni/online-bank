682ab9186560dd77a963413b7fc2cd95bfcf34e4
ongoni <ongoni.fg@gmail.com>
2018-06-14 21:37:46 +0400

    edit web config

--- src/main/kotlin/com/ongoni/onlinebank/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/ongoni/onlinebank/config/WebSecurityConfig.kt
@@ -1,18 +1,13 @@
 package com.ongoni.onlinebank.config
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import javax.sql.DataSource
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
-    @Autowired
-    lateinit var dataSource: DataSource
-
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
         http
@@ -23,6 +18,6 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 .formLogin().loginPage("/login")
                 .defaultSuccessUrl("/home").permitAll()
                 .and()
-                .logout().permitAll()
+                .logout().logoutSuccessUrl("/home").permitAll()
     }
 }
